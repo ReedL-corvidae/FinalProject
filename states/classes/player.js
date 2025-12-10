1dbc910c6c3b930f950a78fcfed14e0905fbe1cb
ReedL-corvidae <rclinehan@email.neit.edu>
2025-12-10 01:39:38 -0500

    collision, health, losing and dodging added

--- states/classes/player.js
+++ states/classes/player.js
@@ -20,10 +20,33 @@ export class Player{
         this.bulletOffset = 12;
         this.direction = direction;
 
+        //Mega bullet things too
+        this.isCharging = false;
+        this.chargeStartTime = 0;
+        this.chargeDuration = 500; //this is 1/2 second
+
+        //health
+        this.health = 3;
+
+        //bullet cooldowns
+        this.lastShot = 0;
+        this.fireCooldown = 300;
+
+        //Dodging things
+        this.isDoding = false;
+        this.dodgeCooldown = 2000;
+        this.lastDodge = 0;
+        this.dodgeDistance = 60;
+
     }
 
     //allows movement for the player
     updateMove(){
+
+        if (this.isCharging) {
+            return;
+        }
+
         if (this.moveUp){
             this.y -= this.speed;
         }
@@ -63,7 +86,42 @@ export class Player{
         }
     }
 
+    //adds a dodging function
+    dodge(){
+        let now = Date.now();
+
+        if(now-this.lastDodge < this.dodgeCooldown) {
+            return;
+        }
+
+        this.lastDodge = now;
+        this.isDodging = true;
+
+        //moves the player a random direction up or down
+        let randomDirection = Math.random() < .5 ? -1 : 1;
+
+        this.y += randomDirection * this.dodgeDistance;
+
+        if(this.y < 0) {
+            this.y = 0;
+        }
+        if(this.y + this.height > this.gameHeight) {
+            this.y = this.gameHeight - this.height;
+        }
+            this.isDodging = false;
+    }
+
     shoot(){
+
+        //dynamic cooldown that bases the actual date/time to the last shot and cooldown
+        let now = Date.now();
+
+        if(now - this.lastShot < this.fireCooldown){
+            return null;
+        }
+
+        this.lastShot = now;
+    
         //sets the bullet location from the player
         let bulletX;
 
@@ -77,6 +135,20 @@ export class Player{
 
         return new Bullet(bulletX, bulletY, this.direction);
        
+    }
+
+    shootMega() {
+        let bulletX = this.direction == 1 ? this.x + this.width: this.x - 40;
+
+        let bulletY = this.y + this.height / 2;
+
+        let mega = new Bullet(bulletX, bulletY, this.direction);
+
+        mega.width = 30;
+        mega.height = 10;
+        mega.speed = 12;
+
+        return mega;
     }
 
 }
--- states/game.js
+++ states/game.js
@@ -1,10 +1,12 @@
 import { Player } from "./classes/player.js";
+import { Toolbox } from"../toolbox.js";
 export class Game{
     
-    
+    toolbox = new Toolbox();
 
     canvas;
     pencil;
+    changeToGame = false;
 
     constructor(canvas) {
 
@@ -16,11 +18,13 @@ export class Game{
         this.player1 = new Player(20, canvas.height/2-40, 20, 48, 5, "w", "s", 1, canvas);
         this.player2 = new Player(canvas.width - 40, canvas.height/2-40, 20, 48, 5, "ArrowUp", "ArrowDown", -1, canvas);
         
+        //store thine bullets
         this.bullets = [];
+
+        //makes everything, literally everything work.
         
         this.setupInputs();
         this.update();
-
    
     }
     //properly detects player inputs for movement
@@ -29,24 +33,90 @@ export class Game{
         document.addEventListener("keydown", e =>{
             this.player1.handleKeyDown(e.key);
             this.player2.handleKeyDown(e.key);
+
+            //shoot bullets
+
+            if (e.key == "d" || e.key == "D"){
+                let bullet = this.player1.shoot();
+
+                //prevents NULL from entering into bullet array
+                if (bullet) this.bullets.push(bullet);
+            }
+            if (e.key == "ArrowLeft"){
+                let bullet = this.player2.shoot();
+
+                //prevents NULL from entering into bullet array
+                if (bullet) this.bullets.push(bullet);
+            }
+
+            if(e.key == "a") {
+                this.player1.dodge();
+            }
+            if(e.key == "ArrowRight"){
+                this.player2.dodge();
+            }
+
+            //mega bullet checking
+
+            if(e.key == "e"){
+                if(!this.player1.isCharging){
+                    this.player1.isCharging = true;
+                    this.player1.chargeStartTime = performance.now();
+                }
+            } 
+            if(e.key == "/"){
+                if(!this.player2.isCharging){
+                    this.player2.isCharging = true;
+                    this.player2.chargeStartTime = performance.now();
+                }
+            } 
         });
         document.addEventListener("keyup", e => {
             this.player1.handleKeyUp(e.key);
             this.player2.handleKeyUp(e.key);
+
+            //mega bullet
+
+            if(e.key == "e"){
+                this.tryMegaShot(this.player1);
+            }
+            if(e.key == "/"){
+                this.tryMegaShot(this.player2);
+            }
         });
 
-        window.addEventListener("keydown", (e) => {
-            if (e.key == "d" || e.key == "D"){
-                let bullet = this.player1.shoot();
-                this.bullets.push(bullet);
-            }
-            if (e.key == "ArrowLeft"){
-                let bullet = this.player2.shoot();
-                this.bullets.push(bullet);
-            }
-        });
     }
-    //draws players and such
+    //draws players and health and such
+
+    drawHealth() {
+        for(let i = 0; i < this.player1.health; i++){
+            this.pencil.fillStyle = "red";
+            this.pencil.fillRect(10 + i * 20, this.canvas.height - 20, 15, 15);
+        }
+        for(let i = 0; i<this.player2.health;i++){
+            this.pencil.fillstyle = "red";
+            this.pencil.fillRect(this.canvas.width - 10 - (i + 1) * 20, this.canvas. height - 20, 15, 15);
+        }
+    }
+
+    tryMegaShot(player){
+        if(!player.isCharging) return;
+
+        player.isCharging = false;
+
+        let heldTime = performance.now() - player.chargeStartTime;
+
+        if (heldTime >= player.chargeDuration){
+            let mega = player.shootMega();
+
+            //the lazy way to make it do 2 damage :)
+            this.bullets.push(mega);
+            this.bullets.push(mega);
+        } else {
+            //vast nothingness.
+        }
+    }
+
     update(){
         console.log("In game!");
 
@@ -61,6 +131,37 @@ export class Game{
         //draws bullets and adjusts bullet array
         this.bullets.forEach(bullet => bullet.draw(this.pencil) & bullet.update());
 
-        this.bullets = this.bullets.filter(bullet => bullet.x > 0 && bullet.x <  this.canvas.width);
+        this.bullets = this.bullets.filter(bullet => {
+
+        if(this.toolbox.isWithinRect(bullet.x, bullet.y, this.player1.x, this.player1.y, this.player1.width, this.player1.height)){
+                this.player1.health - 2;
+            console.log("player 1 is hit");
+            this.player1.health--;
+            console.log(this.player1.health);
+            return false;
+        }
+        if(this.toolbox.isWithinRect(bullet.x, bullet.y, this.player2.x, this.player2.y, this.player2.width, this.player2.height)){
+            console.log("player 2 is hit");
+            this.player2.health--;
+            console.log(this.player2.health);
+            return false;
+        }
+
+        return true;
+    });
+
+    //Checks if player's health is below or equal to 0, if it is, it changes the changeToGame to true
+
+    if(this.player1.health <=0 || this.player2.health <= 0){
+        this.changeToGame = true;
+    }
+    //changes the screen to the game over screen.
+    if(this.changeToGame) {
+            this.changeToGame = false; //consume it; so we reset the title screen for next time.
+            return "gameOver";
+        }
+
+    //Draws both player's health
+     this.drawHealth();
     }
 }
